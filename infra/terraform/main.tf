0ea42bc0a2d157856d13ff69bbeb708554593f19
puppen meisterin <contact@rinnadia.uk>
2026-08-11 20:07:46 +0900

    chore: fix indentation in main.tf

--- infra/terraform/main.tf
+++ infra/terraform/main.tf
@@ -19,13 +19,12 @@ terraform {
   }
 
   # NOTE: this bucket must already exist before you run `terraform init`.
-  # Create it once by hand, then uncomment this block.
   backend "s3" {
-     bucket       = "rin-ecommerce-serverless-order-pipeline-930388172524"
-     key          = "order-pipeline/terraform.tfstate"
-     region       = "ap-southeast-2"
-     encrypt      = true
-     use_lockfile = true
+    bucket       = "rin-ecommerce-serverless-order-pipeline-930388172524"
+    key          = "order-pipeline/terraform.tfstate"
+    region       = "ap-southeast-2"
+    encrypt      = true
+    use_lockfile = true
   }
 }
 
